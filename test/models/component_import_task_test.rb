216456a35abaa973039fee9cb52149b124afa9e3
Austin Welch <aikepah@gmail.com>
2025-08-20 10:08:33 -0600

    Apply rubocop fixes

--- test/models/component_import_task_test.rb
+++ test/models/component_import_task_test.rb
@@ -1,34 +1,34 @@
-require 'test_helper'
-require 'rake'
+require "test_helper"
+require "rake"
 
 class ComponentImportTaskTest < ActiveSupport::TestCase
   def setup
-    Rake.application.rake_require('tasks/component_import', [Rails.root.join('lib')])
+    Rails.application.load_tasks
     Rake::Task.define_task(:environment)
     Component.delete_all
     # Seed a few components for import to update
-    Component.create!(monster_type: 'aberration', component_type: 'bone', dc: 1, edible: false, volatile: false, note: '')
-    Component.create!(monster_type: 'beast', component_type: 'eye', dc: 1, edible: false, volatile: false, note: '')
+    Component.create!(monster_type: "aberration", component_type: "bone", dc: 1, edible: false, volatile: false, note: "")
+    Component.create!(monster_type: "beast", component_type: "eye", dc: 1, edible: false, volatile: false, note: "")
   end
 
   def test_import_from_yaml_updates_components
     assert_difference -> { Component.where(dc: 10, edible: true).count }, +1 do
-      Rake::Task['components:import_from_yaml'].reenable
-      Rake::Task['components:import_from_yaml'].invoke
+      Rake::Task["components:import_from_yaml"].reenable
+      Rake::Task["components:import_from_yaml"].invoke
     end
-    aberration_bone = Component.find_by(monster_type: 'aberration', component_type: 'bone')
+    aberration_bone = Component.find_by(monster_type: "aberration", component_type: "bone")
     assert_equal 10, aberration_bone.dc
     assert_equal true, aberration_bone.edible
     assert_equal false, aberration_bone.volatile
-    assert_equal '', aberration_bone.note
+    assert_equal "", aberration_bone.note
   end
 
   def test_import_from_yaml_skips_missing_components
     # Should not raise error if a component is missing
-    Component.where(monster_type: 'beast', component_type: 'eye').delete_all
-    Rake::Task['components:import_from_yaml'].reenable
+    Component.where(monster_type: "beast", component_type: "eye").delete_all
+    Rake::Task["components:import_from_yaml"].reenable
     assert_nothing_raised do
-      Rake::Task['components:import_from_yaml'].invoke
+      Rake::Task["components:import_from_yaml"].invoke
     end
   end
 end
